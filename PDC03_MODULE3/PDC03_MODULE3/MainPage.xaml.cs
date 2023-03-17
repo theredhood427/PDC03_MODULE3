ed1d0b55e6954e8e3d419d6ac53f9c129c81f14d
theredhood427 <bangsil.ronrusselle@auf.edu.ph>
2023-03-17 10:40:00 +0800

    first commit

---
+++ PDC03_MODULE3/PDC03_MODULE3/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDC03_MODULE3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Activity1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity1());
+        }
+        private async void Activity2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity2());
+        }
+        private async void Activity3(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity3());
+        }
+        private async void Activity4(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity4());
+        }
+    }
+}
